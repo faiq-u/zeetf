964564be9b34f1e16c48f91fcadb9b4e8a4a20d7
myname-png <122798468+myname-png@users.noreply.github.com>
2023-06-19 00:33:59 +0500

    Add files via upload

--- fathersday/settings.py
+++ fathersday/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a*or$5-)n3qo6a)zf+ac#vtfuvyi29^)&%k3)#6p+02v9eaz&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app','now.sh']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
